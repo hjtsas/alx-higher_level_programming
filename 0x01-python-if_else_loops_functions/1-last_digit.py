1caa283e81b7d794ec43c17cf68102048d21b35f
Hoja Tesema <hojats@hotmail.com>
2023-08-10 05:06:58 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,8 +3,8 @@ import random
 number = random.randint(-10000, 10000)
 digit = number %10
 if digit > 5:
-    print("{} is {} and is greater than 5".format(number, digit))
+    print("The last digit of {} is {} and is greater than 5".format(number, digit))
 elif digit == 0:
-    print("{} is {} and is 0".format(number, digit))
+    print("The last digit of {} is {} and is 0".format(number, digit))
 else:
-    print("{} is {} and is less than 6 and not 0".format(number, digit)))
+    print("The last digit of {} is {} and is less than 6 and not 0".format(number, digit)))

b5eb2cc3364db4a6084d22810926fdac67d1a230
Hoja Tesema <hojats@hotmail.com>
2023-08-12 13:14:02 -0700

    102-magic_calculation.py

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def magic_calculation(a, b):
+    add = 0
+    sub = 0
+    c = 0
+
+    if a < b:
+        add = __import__('magic_calculation_102').add
+        c = add(a, b)
+        for i in range(4, 6):
+            c = add(c, i)
+        return c
+    else:
+        sub = __import__('magic_calculation_102').sub
+        return sub(a, b)

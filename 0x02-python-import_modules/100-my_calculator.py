fb3029dbeee58dfd3e799fcb307b0d551f5eaf2a
Hoja Tesema <hojats@hotmail.com>
2023-08-12 13:04:09 -0700

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -8,10 +8,13 @@ if __name__ == "__main__":
     if len(sys.argv) - 1 != 3:
         print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         sys.exit(1)
+
     opr = {'+':add, '-':sub, '/':div, '*':mul}
+
     if sys.argv[2] not in list(opr.keys()):
         print('Unknown operator. Available operators: +, -, * and /')
         sys.exit(1)
+
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     print('{} {} {} = {}'.format(a,sys.argv[2], b, opr[sys.argv[2]](a, b)))

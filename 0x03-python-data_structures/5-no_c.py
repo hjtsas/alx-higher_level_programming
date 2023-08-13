8cb32c581b14ca8051d3ca809c091a9e2270b595
Hoja Tesema <hojats@hotmail.com>
2023-08-13 11:47:18 -0700

    5-no_c.py

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -2,5 +2,5 @@
 
 def no_c(my_string):
 
-    new_string = [x for x in my_string if x != 'C' and x != 'c']
+    new_string = [x for x in my_string if x != 'c' and x != 'C']
         return ("".join(new_string))

cb5543538b67460b71f8526ef671481f3650326a
Hoja Tesema <hojats@hotmail.com>
2023-08-13 10:31:58 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -5,5 +5,6 @@ def new_in_list(my_list, idx, element):
     if idx < 0 or idx > len(my_list):
         return my_list
 
-    my_list[idx] = element
+    new_list = [x for x in my_list]
+    new_list[idx] = element
     return my_list

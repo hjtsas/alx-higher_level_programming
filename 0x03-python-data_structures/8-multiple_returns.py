23f83f96d3bff3f021621785e99010b35e57b84e
Hoja Tesema <hojats@hotmail.com>
2023-08-14 05:34:37 -0700

    8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -2,6 +2,6 @@
 
 def multiple_returns(sentence):
 
-    if (len(sentence) - 1) == 0:
-        return None
-    print('Length: {:d} - First character: {}'.format((len(sentence)-1), sentence[:0]))
+    if len(sentence) == 0:
+        return (0, None)
+    print('Length: {:d} - First character: {}'.format(len(sentence), sentence[0]))

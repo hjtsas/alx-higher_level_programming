e3be541d3844e643b400d3b07c537eea02a659bd
Hoja Tesema <hojats@hotmail.com>
2023-08-14 05:19:58 -0700

    7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return tuple(map(sum, zip(tuple_a, tuple_b)))
+
+    t1 = tuple_a[:2] + (0,) * (2 - len(tuple_a))
+    t2 = tuple_b[:2] + (0,) * (2 - len(tuple_b))
+    return tuple(map(sum, zip(t1, t2)))

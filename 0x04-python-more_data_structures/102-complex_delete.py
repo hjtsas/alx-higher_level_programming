71478f4fc37218906591867d4cc3d242d4daa520
Hoja Tesema <hojats@hotmail.com>
2023-08-19 12:17:20 -0700

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -4,5 +4,5 @@ def complex_delete(a_dictionary, value):
     for i in list(a_dictionary.keys()):
         if a_dictionary[i] == value:
             del a_dictionary[i]
-    return a_dictionary
+    return (a_dictionary)
 

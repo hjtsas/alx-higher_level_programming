b94d60e4c26f79feff1298da96027901b930e61c
Hoja Tesema <hojats@hotmail.com>
2023-08-28 00:53:26 -0700

    0-safe_print_list.py

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in my_list:
+        try:
+            if count < x:
+                print(i, end="")
+                count += 1
+            else:
+                break
+            except:
+                pass
+            print()
+            return count

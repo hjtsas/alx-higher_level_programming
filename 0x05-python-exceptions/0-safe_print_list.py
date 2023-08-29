a6c2118c8388054cea6b30a51e371038d4444ef8
Hoja Tesema <hojats@hotmail.com>
2023-08-29 05:35:34 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-     try:
-         print("{:d}".format(value))
-         return (True)
-     except (TypeError, ValueError):
-         return (False)
+    ret = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)

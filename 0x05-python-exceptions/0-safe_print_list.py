ff6a198c72a1968c992704f6c35cef22c71c8b1b
Hoja Tesema <hojats@hotmail.com>
2023-08-29 05:33:31 -0700

    0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,25 +1,8 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-     """Print an integer with "{:d}".format().
-
-    Args:
-        value (int): The integer to print.
-
-    Returns:
-        If a TypeError or ValueError occurs - False.
-        Otherwise - True.
-    """
-
-    count = 0
-    for i in my_list:
-        try:
-            if count < x:
-                print(i, end="")
-                count += 1
-            else:
-                break
-            except:
-                pass
-            print()
-            return count
+     try:
+         print("{:d}".format(value))
+         return (True)
+     except (TypeError, ValueError):
+         return (False)

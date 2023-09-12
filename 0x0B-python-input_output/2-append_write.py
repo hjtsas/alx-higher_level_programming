b0d4297ffdab71766ccc875bc2a68118a7d437d6
Hoja Tesema <hojats@hotmail.com>
2023-09-12 10:34:18 -0700

    2-append_write.py

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+"""Defines a text file-Append function"""
+def append_write(filename="", text=""):
+     """Append the contents of a UTF8 text file"""
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return f.write(text)

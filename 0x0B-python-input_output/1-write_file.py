757f2cb97a729aedbe675260b2d18a107d107b74
Hoja Tesema <hojats@hotmail.com>
2023-09-12 10:28:39 -0700

    1-write_file.py

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Defines a text file-Write function"""
+def write_file(filename="", text=""):
+    """Write the contents of a UTF8 text file"""
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.write(text)
